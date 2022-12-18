3c16cdb36f2fbcfb704c76ee6210c44c903b2d63
Yisheng Gong <yisheng_gong@onmail.com>
2022-12-17 19:18:54 -0500

    add test case

---
+++ bm/test.py
@@ -0,0 +1,13 @@
+import bm
+
+def test_match():
+    assert(bm.match("ddd", "ddd")==0)
+    assert(bm.match("abcde", "abc")==0)
+    assert(bm.match("ecdab", "ab")==3)
+    assert(bm.match("ecdabababababc", "abc")==11)
+    assert(bm.match("asjdiooeac", "X")==-1)
+    assert(bm.match("awsxedc", "awsxedcs")==-1)
+    print("test passed")
+
+if __name__ == "__main__":
+    test_match()
