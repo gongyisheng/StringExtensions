46e6284e45554a6c19045e25c50a381d68a1c1ef
Yisheng Gong <yisheng_gong@onmail.com>
2022-12-17 19:19:57 -0500

    add test case

--- bm/test.py
+++ bm/test.py
@@ -2,6 +2,7 @@ import bm
 
 def test_match():
     assert(bm.match("ddd", "ddd")==0)
+    assert(bm.match("dddd", "ddd")==0)
     assert(bm.match("abcde", "abc")==0)
     assert(bm.match("ecdab", "ab")==3)
     assert(bm.match("ecdabababababc", "abc")==11)
